5933158840e2483bca3bffe0707228137da36241
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-07-13 08:37:50 +0200

    id bugfix

--- src/app/company.service.ts
+++ src/app/company.service.ts
@@ -8,14 +8,17 @@ export class CompanyService {
   private selectedCompany: Company | undefined;
   private companies: Company[] = [];
   private storageKey = 'companies';
-  private currentId = 0;
+  private currentId: number = 100;
 
   constructor() {
     this.loadCompaniesFromStorage();
+    this.loadCurrentIdFromStorage();
   }
 
   private generateUniqueId(): number {
-    return this.currentId++;
+    this.currentId++;
+    this.saveCurrentIdToStorage();
+    return this.currentId;
   }
 
   setCompanies(companies: Company[]): void {
@@ -29,11 +32,10 @@ export class CompanyService {
 
   addCompany(company: Company): void {
     const newId = this.generateUniqueId();
-    company.id = newId; 
+    company.id = newId;
     this.companies.push(company);
     this.saveCompaniesToStorage();
   }
-  
 
   setSelectedCompany(company: Company): void {
     this.selectedCompany = company;
@@ -53,6 +55,16 @@ export class CompanyService {
       this.companies = JSON.parse(storedCompanies);
     }
   }
+
+  private loadCurrentIdFromStorage(): void {
+    const storedId = localStorage.getItem('currentId');
+    this.currentId = storedId ? parseInt(storedId, 10) : 0;
+  }
+
+  private saveCurrentIdToStorage(): void {
+    localStorage.setItem('currentId', this.currentId.toString());
+  }
+
   getCompanyById(id: number): Company | undefined {
     return this.companies.find(company => company.id === id);
   }
@@ -64,3 +76,4 @@ export class CompanyService {
     }
   }
 }
+
